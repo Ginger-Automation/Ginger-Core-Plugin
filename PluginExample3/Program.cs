20bdb3c5b4719c35539cde4de45db5d7fdc7e89e
Yaron Weiss <YARON.WEISS@amdocs.com>
2018-10-01 21:02:44 -0500

    Change in Plugin examples for new style + v 2.6.6

---
+++ PluginExample3/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amdocs.Ginger.Plugin.Core;
+using System;
+
+namespace PluginExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Starting Plugin Exmaple 3");
+            GingerNodeStarter.StartNode(new TextFileService(), "Text File Service 1");            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PluginExample3/TextFileService.cs
@@ -0,0 +1,34 @@
+﻿using Amdocs.Ginger.Plugin.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginExample3
+{
+    [GingerService("TextFileService", "Text File service")]
+    public class TextFileService : IGingerService
+    {
+        [GingerAction("WordsCount", "Count number of words in text file")]
+        public void Sum(IGingerAction GA, string fileName)
+        {            
+            //In
+            if (!System.IO.File.Exists(fileName))
+            {
+                GA.AddError("File not found: " + fileName);
+            }
+
+            //Act
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+            int count = 0;
+            foreach (string l in lines)
+            {
+                count += l.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+            }
+
+            //Out
+            GA.AddOutput("count", count);
+            
+            GA.AddExInfo("Count=" + count);
+        }
+    }
+}
