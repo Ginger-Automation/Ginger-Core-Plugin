78b686d24c196328d6614d4b1a8ad71a6ba78e66
Yaron Weiss <yaron.weiss@amdocs.com>
2019-01-10 16:50:31 -0600

    Fix example TextFileService plugin

--- PluginExample3/Program.cs
+++ PluginExample3/Program.cs
@@ -7,8 +7,21 @@ namespace PluginExample3
     {
         static void Main(string[] args)
         {            
-            Console.WriteLine("Starting Plugin Exmaple 3");
-            GingerNodeStarter.StartNode(new TextFileService(), "Text File Service 1");            
+            Console.WriteLine("Starting Plugin Exmaple 3");            
+            using (GingerNodeStarter gingerNodeStarter = new GingerNodeStarter())
+            {
+                if (args.Length > 0)
+                {
+                    gingerNodeStarter.StartFromConfigFile(args[0]);  // file name 
+                }
+                else
+                {                    
+                    gingerNodeStarter.StartNode("Text File Service 1", new TextFileService());
+                }
+                gingerNodeStarter.Listen();
+            }
+
+
             Console.ReadKey();
         }
     }
